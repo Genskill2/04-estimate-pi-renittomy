791b772839dc5097e1a523ca331945e52567e9cf
renittomy <84716827+renittomy@users.noreply.github.com>
2021-06-01 23:30:46 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -39,5 +39,18 @@ int main(void) {
   }
 }
 
-
-
+float mc_pi(int n)
+{
+  float ret;
+  int countin=0;
+  int countout=0
+  float x= float frandom();
+  float y= float frandom();
+  float dist=sqrt(x*x+y*y);
+  if(dist<=1)
+    countin++;
+  else 
+    countout++;
+  ret=4*countin/countout;
+  return ret;
+} 
